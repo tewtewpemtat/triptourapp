fe944122ed93e7acbf3fcfcb6e78efc0a1e06415
tewtewpemtat <110074190+tewtewpemtat@users.noreply.github.com>
2023-11-16 21:54:54 +0700

    addplace downplace slideplace

--- lib/addplace/slideplace.dart
+++ lib/addplace/slideplace.dart
@@ -1,28 +1,87 @@
 import 'package:flutter/material.dart';
-import '../authen/login.dart';
 
-class TopNavbar extends StatelessWidget implements PreferredSizeWidget {
+class SlidePlace extends StatefulWidget {
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  _SlidePlaceState createState() => _SlidePlaceState();
+}
+
+class _SlidePlaceState extends State<SlidePlace> {
+  List<String> placeTypes = [
+    'ร้านกาแฟ',
+    'ทะเล',
+    'ธรรมชาติ',
+    'สวนสนุก',
+    'อาหาร',
+    'ร้านสตรีทฟู้ด',
+    'วัดโบส',
+    'ภูเขา',
+  ];
+
+  String selectedPlaceType = 'ร้านกาแฟ';
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text('Travel Together'),
-      actions: [
-        IconButton(
-          icon: Icon(Icons.account_circle),
-          onPressed: () {
-            // ไปยังหน้าโปรไฟล์
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) =>
-                      LoginPage()), // RegisterPage() คือหน้าที่คุณต้องไป
-            );
-          },
-        ),
-      ],
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      margin: EdgeInsets.all(0.0),
+      height: 100,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 1.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'ประเภทสถานที่',
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: placeTypes.map((type) {
+                return _buildPlaceType(type);
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  Widget _buildPlaceType(String placeType) {
+    bool isSelected = selectedPlaceType == placeType;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedPlaceType = placeType;
+        });
+      },
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        margin: EdgeInsets.only(right: 5.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 1.0),
+          borderRadius: BorderRadius.circular(5.0),
+          color: isSelected ? Colors.yellow : Colors.white,
+        ),
+        child: Text(
+          placeType,
+          style: TextStyle(fontSize: 14.0),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: SlidePlace(),
+    ),
+  ));
 }
