075178a52a9198ab5df661efa2dd9cf2f2cc6a3b
ChayanrotJA <s6303051613084@email.kmutnb.ac.th>
2023-11-20 00:21:39 +0700

    Update

---
+++ assets/lib/addplace.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+import 'addplace/downplace.dart';
+import 'addplace/slideplace.dart';
+
+class AddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(0),
+            child: Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color(0xffeaeaea),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Row(
+                  children: [
+                    Icon(Icons.search, color: Colors.grey),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'ค้นหาสถานที่',
+                          border: InputBorder.none,
+                          isDense: true,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        color: Color(0xFFF0F0F0), // เพิ่มบรรทัดนี้
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'เพิ่มสถานที่',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'เพิ่มสถานที่บนทริปของคุณ',
+                style: TextStyle(
+                  fontSize: 14.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: SlidePlace(),
+            ),
+            Expanded(
+              child: DownPage(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: AddPage(),
+  ));
+}
