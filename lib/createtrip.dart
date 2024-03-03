e9957db46dcfacf04191c978d8427fe2645798a3
tewtewpemtat <110074190+tewtewpemtat@users.noreply.github.com>
2024-03-03 19:16:18 +0700

    tripcreate

--- lib/createtrip.dart
+++ lib/createtrip.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:triptourapp/main.dart';
-
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'tripmanage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class CreateTripPage extends StatefulWidget {
   @override
@@ -13,9 +15,14 @@ class CreateTripPage extends StatefulWidget {
 }
 
 class _CreateTripPageState extends State<CreateTripPage> {
+  bool _isLoading = false;
+
   File? _userProfileImage;
   DateTime selectedStartDate = DateTime.now();
   DateTime selectedEndDate = DateTime.now();
+  final TextEditingController _tripNameController = TextEditingController();
+  String? uid = FirebaseAuth.instance.currentUser?.uid;
+
   int? selectedParticipants = 1;
 
   Future<void> _pickImage() async {
@@ -25,6 +32,22 @@ class _CreateTripPageState extends State<CreateTripPage> {
       setState(() {
         _userProfileImage = File(pickedFile.path);
       });
+    }
+  }
+
+  Future<String?> _uploadImage() async {
+    if (_userProfileImage != null) {
+      String tripName = _tripNameController.text; // Get trip name
+      String fileName =
+          'trip/profiletrip/$uid/$tripName.jpg'; // Construct file name
+      firebase_storage.Reference ref =
+          firebase_storage.FirebaseStorage.instance.ref().child(fileName);
+
+      await ref.putFile(_userProfileImage!);
+      String imageUrl = await ref.getDownloadURL();
+      return imageUrl;
+    } else {
+      return 'assets/cat.jpg';
     }
   }
 
@@ -43,6 +66,51 @@ class _CreateTripPageState extends State<CreateTripPage> {
           selectedEndDate = picked;
         }
       });
+    }
+  }
+
+  Future<void> _createTrip() async {
+    setState(() {
+      _isLoading = true; // Show loading spinner
+    });
+
+    String? imageUrl =
+        await _uploadImage(); // Wait for image upload to complete
+
+    if (imageUrl != null) {
+      // Image upload successful, proceed to create trip
+      String tripName = _tripNameController.text;
+      // You can add more fields as per your requirements
+      DateTime tripStartDate = selectedStartDate;
+      DateTime tripEndDate = selectedEndDate;
+      int tripLimit = selectedParticipants ?? 1;
+      String tripStatus = "กำลังดำเนินการ";
+      List<String> tripJoin = [];
+
+      // Add trip data to Firestore
+      await FirebaseFirestore.instance.collection('trips').add({
+        'tripCreate': uid,
+        'tripName': tripName,
+        'tripProfileUrl': imageUrl,
+        'tripStartDate': tripStartDate,
+        'tripEndDate': tripEndDate,
+        'tripLimit': tripLimit,
+        'tripStatus': tripStatus,
+        'tripJoin': tripJoin,
+        // Add more fields as needed
+      });
+
+      // Navigate to TripmanagePage or any other page after trip creation
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => TripmanagePage(),
+        ),
+      );
+    } else {
+      // Image upload failed, handle error or show message to user
+      // You can handle error scenario here
+      print("Image upload failed.");
     }
   }
 
@@ -78,12 +146,19 @@ class _CreateTripPageState extends State<CreateTripPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              CircleAvatar(
-                radius: 100.0,
-                backgroundImage: _userProfileImage != null
-                    ? FileImage(_userProfileImage!) as ImageProvider<Object>?
-                    : AssetImage('assets/cat.jpg'),
-                child: InkWell(
+              Container(
+                width: 200.0,
+                height: 200.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: _userProfileImage != null
+                        ? FileImage(_userProfileImage!)
+                        : AssetImage('assets/cat.jpg') as ImageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: GestureDetector(
                   onTap: _pickImage,
                   child: Icon(
                     Icons.camera_alt,
@@ -92,6 +167,7 @@ class _CreateTripPageState extends State<CreateTripPage> {
                   ),
                 ),
               ),
+
               SizedBox(height: 3),
               Text(
                 "เพิ่มรูปทริป",
@@ -104,6 +180,7 @@ class _CreateTripPageState extends State<CreateTripPage> {
               ),
               SizedBox(height: 10),
               TextFormField(
+                controller: _tripNameController,
                 decoration: InputDecoration(
                   labelText: "ตั้งชื่อทริปของคุณ",
                   border: OutlineInputBorder(
@@ -193,15 +270,25 @@ class _CreateTripPageState extends State<CreateTripPage> {
               ),
               SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {
-                  // ทำงานเมื่อกดปุ่ม "ดำเนินการต่อ"
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => TripmanagePage(),
-                    ),
-                  );
-                },
+                onPressed: _isLoading
+                    ? null
+                    : () {
+                        if (_tripNameController.text.isEmpty) {
+                          // Show error message if trip name is empty
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                'กรุณากรอกชื่อทริป',
+                                textAlign: TextAlign.center,
+                              ),
+                              backgroundColor: Color.fromARGB(255, 2, 2, 2),
+                            ),
+                          );
+                        } else {
+                          // Proceed with trip creation if trip name is not empty
+                          _createTrip();
+                        }
+                      },
                 style: ElevatedButton.styleFrom(
                   primary: Color(0xffdb923c),
                   onPrimary: Colors.white,
@@ -210,35 +297,45 @@ class _CreateTripPageState extends State<CreateTripPage> {
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                child: Text(
-                  "ดำเนินการต่อ",
-                  style: GoogleFonts.ibmPlexSansThai(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            CreateTripPage()), // RegisterPage() คือหน้าที่คุณต้องไป
-                  );
-                },
-                child: Text(
-                  "ล้างข้อมูลเพื่อระบุข้อมูลการสร้างทริปใหม่",
-                  style: GoogleFonts.ibmPlexSansThai(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.blue,
-                    decoration: TextDecoration.underline,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
+                child: _isLoading
+                    ? SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                        ),
+                      )
+                    : Text(
+                        "ดำเนินการต่อ",
+                        style: GoogleFonts.ibmPlexSansThai(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+              )
+
+              // SizedBox(height: 20),
+              // InkWell(
+              //   onTap: () {
+              //     Navigator.push(
+              //       context,
+              //       MaterialPageRoute(
+              //           builder: (context) =>
+              //               CreateTripPage()), // RegisterPage() คือหน้าที่คุณต้องไป
+              //     );
+              //   },
+              //   child: Text(
+              //     "ล้างข้อมูลเพื่อระบุข้อมูลการสร้างทริปใหม่",
+              //     style: GoogleFonts.ibmPlexSansThai(
+              //       fontSize: 14,
+              //       fontWeight: FontWeight.w500,
+              //       color: Colors.blue,
+              //       decoration: TextDecoration.underline,
+              //     ),
+              //     textAlign: TextAlign.center,
+              //   ),
+              // ),
             ],
           ),
         ),
